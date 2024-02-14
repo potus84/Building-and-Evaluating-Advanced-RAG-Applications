dd0690d738fd51e8330c8b13073ecfec77e112dd
potus84 <tuantai.cse@gmail.com>
2024-02-14 09:27:09 +0100

    Merge branch 'improvement'

--- utils.py
+++ utils.py
@@ -1,75 +1,5 @@
 #!pip install python-dotenv
 
-
-import os
-from dotenv import load_dotenv, find_dotenv
-
-import numpy as np
-from trulens_eval import (
-    Feedback,
-    TruLlama,
-    OpenAI
-)
-
-from trulens_eval.feedback import Groundedness
-import nest_asyncio
-
-nest_asyncio.apply()
-
-
-def get_openai_api_key():
-    _ = load_dotenv(find_dotenv())
-
-    return os.getenv("OPENAI_API_KEY")
-
-
-def get_hf_api_key():
-    _ = load_dotenv(find_dotenv())
-
-    return os.getenv("HUGGINGFACE_API_KEY")
-
-openai = OpenAI()
-
-qa_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name="Answer Relevance")
-    .on_input_output()
-)
-
-qs_relevance = (
-    Feedback(openai.relevance_with_cot_reasons, name = "Context Relevance")
-    .on_input()
-    .on(TruLlama.select_source_nodes().node.text)
-    .aggregate(np.mean)
-)
-
-#grounded = Groundedness(groundedness_provider=openai, summarize_provider=openai)
-grounded = Groundedness(groundedness_provider=openai)
-
-groundedness = (
-    Feedback(grounded.groundedness_measure_with_cot_reasons, name="Groundedness")
-        .on(TruLlama.select_source_nodes().node.text)
-        .on_output()
-        .aggregate(grounded.grounded_statements_aggregator)
-)
-
-feedbacks = [qa_relevance, qs_relevance, groundedness]
-
-def get_trulens_recorder(query_engine, feedbacks, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-    )
-    return tru_recorder
-
-def get_prebuilt_trulens_recorder(query_engine, app_id):
-    tru_recorder = TruLlama(
-        query_engine,
-        app_id=app_id,
-        feedbacks=feedbacks
-        )
-    return tru_recorder
-
 from llama_index import ServiceContext, VectorStoreIndex, StorageContext
 from llama_index.node_parser import SentenceWindowNodeParser
 from llama_index.indices.postprocessor import MetadataReplacementPostProcessor
@@ -79,7 +9,9 @@ import os
 
 
 def build_sentence_window_index(
-    document, llm, embed_model="local:BAAI/bge-small-en-v1.5", save_dir="sentence_index"
+    document, llm, 
+    embed_model="local:BAAI/bge-small-en-v1.5", 
+    save_dir="sentence_index"
 ):
     # create the sentence window node parser w/ default settings
     node_parser = SentenceWindowNodeParser.from_defaults(
@@ -118,7 +50,8 @@ def get_sentence_window_query_engine(
     )
 
     sentence_window_engine = sentence_index.as_query_engine(
-        similarity_top_k=similarity_top_k, node_postprocessors=[postproc, rerank]
+        similarity_top_k=similarity_top_k, 
+        node_postprocessors=[postproc, rerank]
     )
     return sentence_window_engine
 
